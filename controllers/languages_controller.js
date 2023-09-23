062b6d5ad00224b5035b2564c543bfad8efba4c5
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-09-23 12:10:29 -0500

    added language seed route to controller

--- controllers/languages_controller.js
+++ controllers/languages_controller.js
@@ -2,4 +2,33 @@ const express = require('express')
 const languages = express.Router()
 const Language = require('../models/language.js')
 
+
+// LANGUAGE/SEED ROUTE
+languages.get('/seed', (req, res) => {
+    Language.insertMany()
+        .then(createdLanguages => {
+            res.json({
+                message: "Seed successful!"
+            })
+        })
+})
+
+
+// Index:
+languages.get('/', (req, res) => {
+    Language.find()
+        .then(foundLanguages => {
+            res.json(foundLanguages)
+        })
+})
+
+// Show:
+languages.get('/:name', (req, res) => {
+    Language.findOne({ name: req.params.name .toLowerCase() })
+        .then(foundLanguage => {
+            res.json(foundLanguage)
+        })
+})
+
+
 module.exports = languages
